6c9d17d10d6eaf521689a3fc7bd55ab408b36a8b
TiagoRibeiro <tiagofpr@outlook.com>
2022-07-16 19:48:31 -0300

    create folder icon in assets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./componentes/NotificationButton"
+
 function App() {
 return (
+  <>
     <h1> Olá mundo!</h1>
+    <NotificationButton />
+
+    </>
 )
 }
 export default App
